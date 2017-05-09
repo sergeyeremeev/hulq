8ef030cdecb64d5f08df338197ab3957c54adf43
Sergey Eremeev <13.eremeev.sv@gmail.com>
2017-05-09 12:27:12 +0300

    font fixes, responsive styles

--- app/js/landing_v2.js
+++ app/js/landing_v2.js
@@ -9,17 +9,30 @@ import jQuery from 'jquery';
                 .add('.landing-info-block').add('.notify-form__container').add('.landing-2__bottom-content')
                 .add('.landing-2__bottom-title').add('.landing-2__bottom-cut'),
             notifyGoToButton: $('.notify-go-to'),
-            fakeNotifyButton: $('.fake-notify')
+            fakeNotifyButton: $('.fake-notify'),
+            bigTitle: $('.landing-2__bottom-title'),
+            bottomCut: $('.landing-2__bottom-cut'),
+            bottomBg: $('.landing-2__bottom-bg')
         },
 
         init: function () {
+            $(window).on('resize', landingV2.setBottomCutSize);
+
             $(window).on('scroll.animate-content-2', landingV2.animateContent);
             $(window).on('scroll', landingV2.toggleNotifyGoTo);
 
             $(window).scroll();
+            $(window).resize();
 
             landingV2.elements.fakeNotifyButton.on('click', landingV2.notifyTriggerSubmit);
             landingV2.elements.notifyGoToButton.on('click', landingV2.goToNotifyForm);
+        },
+
+        setBottomCutSize: () => {
+            const bigTitleSize = landingV2.elements.bigTitle.height();
+
+            landingV2.elements.bottomCut.height(bigTitleSize);
+            landingV2.elements.bottomBg.attr('style', 'top: ' + bigTitleSize + 'px;');
         },
 
         animateContent: () => {
@@ -49,10 +62,19 @@ import jQuery from 'jquery';
             }
         },
 
-        goToNotifyForm: () => {
+        goToNotifyForm: (e) => {
+            e.preventDefault();
+
+            const distanceScrolled = $(document).scrollTop(),
+                windowHeight = $(window).height(),
+                documentHeight = $(document).height(),
+                distanceTop = distanceScrolled - windowHeight,
+                distanceBottom = (documentHeight - windowHeight) - distanceScrolled,
+                scrollTo = distanceTop < distanceBottom ? windowHeight : documentHeight - windowHeight;
+
             $('html, body').animate(
-                { scrollTop: $(window).height() },
-                300, 'swing'
+                { scrollTop: scrollTo },
+                500, 'swing'
             );
         },
 
